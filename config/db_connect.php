856cbda3540f47f66777891eb39384d3ba5f08d6
ridwanyoku <ridridwan13245@gmail.com>
2024-10-23 21:54:49 +0700

    perubahan pada config

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,15 +1,15 @@
 <?php
 
 // local
-$conn = mysqli_connect("localhost", "root", "", "stock_app");
-if($conn->connect_error) {
-    die("kondisi gagal: " . $conn->connect_error);
-}
-
-//web 
-// $conn = mysqli_connect("localhost", "u408817815_rdw", "Rdwcn132", "u408817815_stock_app");
+// $conn = mysqli_connect("localhost", "root", "", "stock_app");
 // if($conn->connect_error) {
 //     die("kondisi gagal: " . $conn->connect_error);
 // }
+
+//web 
+$conn = mysqli_connect("localhost", "u408817815_rdw", "Rdwcn132", "u408817815_stock_app");
+if($conn->connect_error) {
+    die("kondisi gagal: " . $conn->connect_error);
+}
 
 ?>
